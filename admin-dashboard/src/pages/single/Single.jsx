c2d3f7c0b84351eb902f9ea90f7b393ed6bc9470
Anthony Obiako <anthonyobiakoc@gmail.com>
2023-10-30 17:06:15 +0100

    Done designing Single User Page

--- admin-dashboard/src/pages/single/Single.jsx
+++ admin-dashboard/src/pages/single/Single.jsx
@@ -1,7 +1,59 @@
 import "./single.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/Table/Table";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Frank Ichigo</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">Kurosaki@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+234 901 345 764</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    Bankai St. 123 Soul Avenue Earth
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Bleach</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Expense ( Last 6 Months )" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
